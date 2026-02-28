169db67ea53041417230b7d7d6422f70b94dad92
parvbilla <parvbilla2004@gmail.com>
2026-02-28 22:49:46 +0530

    uploadMiddleware.js

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,24 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF files allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter,
+});
+
+export default upload;
