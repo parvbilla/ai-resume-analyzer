ce9a41d5d0caf67d503e39622fa6c6d041f1a314
parvbilla <parvbilla2004@gmail.com>
2026-02-28 22:39:54 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,17 @@
 import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API Running...");
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log("Server running"));
